6a44b02a6909a17a225693b6af71166b6aa4b345
Perillo <perillostark@hotmail.com>
2014-08-04 12:34:15 -0300

    Projeto

---
+++ GeradorStoredProcedure/GeradorStoredProcedure/Scripts/Base/GSP.js
@@ -0,0 +1,33 @@
+﻿(function (window, document, $) {
+
+    var GSP = function () {
+
+        function dispose() {
+
+        }
+
+        function variaveisIniciar() {
+
+        }
+
+        function eventosIniciar() {
+
+        }
+
+        function iniciarPrivado() {
+            variaveisIniciar();
+
+            eventosIniciar();
+        }
+
+        this.disposePublico = function () {
+            dispose();
+        };
+
+        this.iniciarPublico = function () {
+            iniciarPrivado();
+        };
+    };
+    window.GSP = new GSP();
+    window.GSP.iniciarPublico();
+}(window, document, jQuery));
